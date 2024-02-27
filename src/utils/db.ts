ac08569d0fbe95c1f0005c57c472cf017ddfdd74
tuulikoo <tuuli.kivisaari@gmail.com>
2024-02-27 13:16:59 +0200

    latest

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const mongoConnect = async () => {
   const connection = await mongoose.connect(process.env.DATABASE_URL as string);
-  console.log('DB connected successfully');
+  console.log('DB connected successfully.');
   return connection;
 };
 
